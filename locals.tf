d797fcb85b6cbc814cc56dc3aaeff9fb0d37d2a6
Joakim Olsson <joakim@unbound.se>
2025-01-22 01:42:27 +0100

    chore: bump operator module version
    Made with ❤️️ by updatecli

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  version               = "0.12.1"
+  version               = "0.13.0"
   external_secrets_yaml = file("${path.module}/external-secrets.yaml")
   external_secrets_store_yaml = templatefile("${path.module}/cluster-secret-store.yaml", {
     region = var.region
